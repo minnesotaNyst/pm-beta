2984954c19c9f579e246c3039de2f54384214341
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-13 16:39:51 -0600

    added the embedded link for form.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,33 @@
 /* initialization provided by materialize for the date picker */
-/* document.addEventListener('DOMContentLoaded', function () {
-	var elems = document.querySelectorAll('.datepicker');
-	var instances = M.Datepicker.init(elems, options);
+/* $(document).ready(function () {
+	$('.datepicker').datepicker();
 }); */
 
-$(document).ready(function () {
-	$('.datepicker').datepicker();
-});
+/* $('#icon_telephone').inputmask({ mask: '(999) 999-9999' }); */
+// studio3T
+
+/* sample entry for the array/objects */
+/* var entry =[
+    {
+    mission: 1234
+    },
+    {
+    mission_date: new Date()
+    },
+    {
+    filled_in: true
+    },
+    {
+    date:
+    }
+
+];
+
+var entries = [missioninfo, general, peds] */
+
+window.onload = function () {
+	Formio.createForm(
+		document.getElementById('formio'),
+		'https://atelyxbavvhqhre.form.io/pmbeta'
+	);
+};
