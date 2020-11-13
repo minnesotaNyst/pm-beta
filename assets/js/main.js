67f85771cbbabb27068acbef153eb48cca7a23fd
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-13 17:53:47 -0600

    embedded the form io and started working on the style sheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,9 @@
 /* initialization provided by materialize for the date picker */
-/* $(document).ready(function () {
+$(document).ready(function () {
 	$('.datepicker').datepicker();
-}); */
+});
 
-/* $('#icon_telephone').inputmask({ mask: '(999) 999-9999' }); */
-// studio3T
-
-/* sample entry for the array/objects */
-/* var entry =[
-    {
-    mission: 1234
-    },
-    {
-    mission_date: new Date()
-    },
-    {
-    filled_in: true
-    },
-    {
-    date:
-    }
-
-];
-
-var entries = [missioninfo, general, peds] */
-
+/* load the form.io when the page loads */
 window.onload = function () {
 	Formio.createForm(
 		document.getElementById('formio'),
