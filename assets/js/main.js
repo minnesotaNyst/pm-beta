2eac409348945c5fc8b267193069cc9625e6fbc1
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-15 18:00:06 -0600

    name now displays in the footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,45 @@ window.onload = function () {
 };
 
 /* load the modal when the page loads */
-$(document).ready(function () {
-	$('.modal').modal();
-	$('#modal1').modal('open');
-	$('.modal-close').click(function () {
-		$('#modal1').modal('close');
+var displayModal = function () {
+	$(document).ready(function () {
+		$('.modal').modal();
+		$('#modal1').modal('open');
+		$('.modal-close').click(function () {
+			$('#modal1').modal('close');
+		});
 	});
-});
-
+};
 /* initialization provided by materialize for the date picker */
 $(document).ready(function () {
 	$('.datepicker').datepicker();
 });
+
+/* get the name from the modal & set variables */
+
+var fName = [];
+
+/* get from local storage */
+var displayName = function () {
+	fName = JSON.parse(localStorage.getItem('firstName'));
+	console.log(fName);
+
+	if (!fName) {
+		fName = [];
+		return;
+	} else {
+		$('#visitor').text(fName);
+	}
+};
+
+/* need to set the name in local storage */
+var setName = function () {
+	var firstName = $('#first_name').val();
+	localStorage.setItem('firstName', JSON.stringify(firstName));
+
+	displayName();
+};
+
+$('#get_name').on('click', setName);
+
+displayModal();
